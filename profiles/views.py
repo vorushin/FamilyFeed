7fe5a97af0effa77f410cf2977f4c33622a8087e
Roman Vorushin <roman.vorushin@gmail.com>
2011-07-30 13:23:30 +0300

    First working page

--- profiles/views.py
+++ profiles/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from django.shortcuts import render
+
+
+def start(request):
+    return render(request, 'profiles/start.html')
+
+
+def registration(request):
+    return render(request, 'profiles/registration.html')
--- urls.py
+++ urls.py
@@ -1,17 +1,10 @@
 from django.conf.urls.defaults import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'FamilyFeed.views.home', name='home'),
-    # url(r'^FamilyFeed/', include('FamilyFeed.foo.urls')),
+    url(r'^', include('profiles.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
