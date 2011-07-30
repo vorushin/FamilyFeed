8f46890b48d1523c89d5084bd2257545d3af3a08
Roman Vorushin <roman.vorushin@gmail.com>
2011-07-30 19:19:43 +0300

    Added make_uri_title

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,11 +1,25 @@
+import logging
+import re
+import sys
+
 from django.contrib.sites.models import Site
+
 
 def log_exception(text, **kwargs):
     kwargs['exc_info'] = sys.exc_info()
     logging.error(text, **kwargs)
+
 
 def absolute_uri(location, request=None):
     return '%s://%s%s' % (
         'https' if request and request.is_secure() else 'http',
         Site.objects.get_current().domain,
         location)
+
+
+def make_uri_title(value):
+    # copied from django.templates.defaultfilters
+    import unicodedata
+    value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
+    value = unicode(re.sub('[^\w\s-]', '', value).strip().lower())
+    return re.sub('[-\s]+', '-', value)
