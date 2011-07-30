c56b2dcfcf1e9414601d80220af7e76e52c79b10
Roman Vorushin <roman.vorushin@gmail.com>
2011-07-30 14:52:27 +0300

    Added fabfile

---
+++ fabfile.py
@@ -0,0 +1,31 @@
+from fabric.api import *
+
+
+def run_in_virtualenv(command):
+    run('source env/bin/activate && ' + command)
+
+#
+# Different servers
+#
+
+
+def test():
+    env.hosts = ['root@hydra.sciworth.com']
+
+
+def production():
+    env.hosts = ['root@173.255.236.60']
+
+#
+# Actual commands
+#
+
+
+def deploy():
+    with cd('/home/django/FamilyFeed'):
+        run('sudo -u django git pull')
+        run_in_virtualenv('pip install -r requirements.txt')
+        run_in_virtualenv('./manage.py syncdb')
+        run_in_virtualenv('./manage.py migrate --noinput')
+        run('initctl reload-configuration')
+        run('restart FamilyFeed')
