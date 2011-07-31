07bc42c577be0204be15a6468c6b4c05b20650a1
Roman Vorushin <roman.vorushin@gmail.com>
2011-08-01 02:40:00 +0300

    Cleaned urls

--- timeline/urls.py
+++ timeline/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('timeline.views',
     url(r'^$', 'start'),
     url(r'^(?P<username>[\w.@+-]+)/$', 'logged_in'),
-    url(r'^(?P<username>[\w.@+-]+)/(?P<child_slug>[\w.@+-]+)$',
+    url(r'^(?P<username>[\w.@+-]+)/(?P<child_slug>[\w.@+-]+)/$',
         'timeline'),
 )
