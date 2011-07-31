f2ee1fb38c9e827147eb448184aaa237272b2e85
Matvey Maslov <matvey@sciworth.com>
2011-07-31 19:35:27 +0300

    timeline: correct urls

--- timeline/urls.py
+++ timeline/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('timeline.views',
     url(r'^$', 'start'),
-    url(r'^timeline/(?P<child_slug>[\w.@+-]+)', 'timeline'),
+    # url(r'^timeline/(?P<child_slug>[\w.@+-]+)', 'timeline'),
+    url(r'^(?P<username>[\w.@+-]+)/(?P<child_slug>[\w.@+-]+)$',
+        'timeline'),
 )
--- timeline/views.py
+++ timeline/views.py
@@ -24,21 +24,19 @@ class YouTubeEvent(object):
 
 def start(request):
     if request.user.is_authenticated():
-        return HttpResponseRedirect(reverse('timeline'))
+        return HttpResponseRedirect(reverse('timeline.views.timeline', args=['vorushin', 'marta']))
     else:
         username = 'vorushin'
         events = json.dumps([YouTubeEvent(video) for video in youtube.list_videos(username)], cls=ObjectEncoder)
         return render(request, 'timeline/start.html', { 'events' : events });
 
-def timeline(request, child_slug):
+def timeline(request, username, child_slug):
     username = 'vorushin'
     events = json.dumps([YouTubeEvent(video) for video in youtube.list_videos(username)], cls=ObjectEncoder)
-    # children = [dict(name='Marta', slug='marta'), 
-    #                  dict(name='Eva', slug='eva')]
     children = [dict(name='Marta', slug='marta', age=age(datetime.datetime(2010, 9, 22))), 
                 dict(name='Eva', slug='eva', age=age(datetime.datetime(2009, 4, 18)))]
     return render(request, "timeline/timeline.html", 
-                  { 'events' : events, 'children' : children, 'current_child_slug' : child_slug})
+                  { 'events' : events, 'children' : children, 'username' : username, 'current_child_slug' : child_slug})
 
 # def events(request):
 #     return HttpResponse(json.dumps(youtube.list_videos(username), cls=ObjectEncoder))
